ea11997d20d2fe29bf78859609dcabbd26082b88
zcaliptium <zcaliptium@gmail.com>
2017-10-24 18:54:48 +0300

    Initial commit with basic files.

---
+++ src/main/java/tws/zcaliptium/hzdslib/common/HazardsLib.java
@@ -0,0 +1,37 @@
+/*******************************************************************************
+ * Copyright (c) 2017 ZCaliptium.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser Public License v3
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-3.0.txt
+ ******************************************************************************/
+package tws.zcaliptium.hzdslib.common;
+
+import net.minecraft.init.Blocks;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = HazardsLib.MODID, version = HazardsLib.VERSION)
+public class HazardsLib
+{
+    public static final String MODID = "hzdslib";
+    public static final String VERSION = "dev";
+    
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+    }
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+    }
+
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+    }
+}
